aa5134bdde0350e23e1779af2661297dd18f387e
syiifahusna <syiifahusna@gmail.com>
2023-11-21 14:34:58 +0800

    add style to pages

---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,30 @@
+
+getYear();
+
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+// owl carousel 
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
