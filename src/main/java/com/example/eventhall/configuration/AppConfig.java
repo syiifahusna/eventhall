187508cd42e84ce3699b146574f43f0165d760a6
syiifahusna <syiifahusna@gmail.com>
2023-11-22 14:59:45 +0800

    add spring security dependency and create security config class

---
+++ src/main/java/com/example/eventhall/configuration/AppConfig.java
@@ -0,0 +1,7 @@
+package com.example.eventhall.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
---
+++ src/main/java/com/example/eventhall/security/AppSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.eventhall.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+}
