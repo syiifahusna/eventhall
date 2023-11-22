3099a56c3e11d86aa6b731aa17531caf138e4f40
syiifahusna <syiifahusna@gmail.com>
2023-11-22 14:45:09 +0800

    create error page and user static pages

---
+++ src/main/java/com/example/eventhall/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.eventhall.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/u")
+public class UserController {
+
+    @Value("${app.user.dir}")
+    private String userDir;
+
+    @GetMapping({"/home","/",""})
+    public String home(){
+        return userDir+"/home";
+    }
+
+    @GetMapping("/hall")
+    public String hall(){
+        return userDir+"/hall";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return userDir+"/about";
+    }
+
+    @GetMapping("/location")
+    public String location(){
+        return userDir+"/location";
+    }
+
+    @GetMapping("/news")
+    public String news(){
+        return userDir+"/news";
+    }
+}
