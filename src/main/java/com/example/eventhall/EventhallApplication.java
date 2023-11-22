c1e4ef715ffaa0ac6682e9f3248c71be1ef4ee1c
syiifahusna <syiifahusna@gmail.com>
2023-11-22 15:41:26 +0800

    create basic security configuration

--- src/main/java/com/example/eventhall/EventhallApplication.java
+++ src/main/java/com/example/eventhall/EventhallApplication.java
@@ -2,12 +2,19 @@ package com.example.eventhall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EventhallApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventhallApplication.class, args);
+		//SpringApplication.run(EventhallApplication.class, args);
+		ConfigurableApplicationContext app =
+				SpringApplication.run(EventhallApplication.class, args);
+		Arrays.stream(app.getBeanDefinitionNames()).forEach(System.out::println);
 	}
+
 
 }
--- src/main/java/com/example/eventhall/security/AppSecurityConfig.java
+++ src/main/java/com/example/eventhall/security/AppSecurityConfig.java
@@ -1,9 +1,43 @@
 package com.example.eventhall.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class AppSecurityConfig {
+public class AppSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests()
+                .requestMatchers("/error").permitAll()
+                .requestMatchers("/",
+                                        "/home",
+                                        "/about",
+                                        "/location",
+                                        "/news",
+                                        "/register",
+                                        "/login"
+                                ).anonymous()
+                .requestMatchers("/u/*").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/u/home")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/logout")
+                .and()
+                .csrf().disable();
+
+
+        return httpSecurity.build();
+
+    }
 }
