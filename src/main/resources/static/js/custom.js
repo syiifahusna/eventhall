87ff1fb97c37d4d89aef08deb307cbd05f816cd0
syiifahusna <syiifahusna@gmail.com>
2023-11-27 17:44:39 +0800

    change all page css to bootstrap 5.2.3

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,17 +1,10 @@
+function pickDate() {
 
-getYear();
+    if(document.getElementById('floatingDateStart') && document.getElementById('floatingDateEnd')){
+        rstartdate = document.getElementById('floatingDateStart')
+        renddate = document.getElementById('floatingDateEnd')
 
-// to get current year
-function getYear() {
-
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-
-    if(document.getElementById('rstartdate') && document.getElementById('renddate')){
-        rstartdate = document.getElementById('rstartdate')
-        renddate = document.getElementById('renddate')
-
+        const currentDate = new Date();
         const today = currentDate.toISOString().split('T')[0];
         rstartdate.setAttribute('min', today);
 
@@ -23,29 +16,3 @@ function getYear() {
 
     }
 }
-
-
-
-// owl carousel 
-
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 6
-        }
-    }
-})
-
-
-
